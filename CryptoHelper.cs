3d1c09dd29bd3a5c6a50a4e0cabb882e39b79382
Hany Bakir <hanybakir@hotmail.com>
2023-12-09 13:05:57 +0300

    Add project files.

---
+++ CryptoHelper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESCryptoTool;
+
+public class CryptoHelper
+{
+
+    private string AESKey = "AAECAwQFBgcICQoLDA0ODw==";
+    private string AESIV = "AAECAwQFBgcICQoL";
+
+    public string AESencrypt(string plainText, string key, string iv)
+    {
+        key = AESKey;
+        iv = AESIV;
+        byte[] encrypted = EncryptStringToBytes_Aes(plainText, Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(iv));
+        return Convert.ToBase64String(encrypted);
+    }
+
+    public string AESdecrypt(string cipherText, string key, string iv)
+    {
+        key = AESKey;
+        iv = AESIV;
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+        string decrypted = DecryptStringFromBytes_Aes(cipherBytes, Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(iv));
+        return decrypted;
+    }
+
+    public string Base64Encode(string plainText)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+
+    public string Base64Decode(string base64EncodedData)
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+
+    byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+        byte[] encrypted;
+
+        // Create an Aes object
+        // with the specified key and IV.
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.KeySize = 128;
+            aesAlg.BlockSize = 128;
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+
+        // Return the encrypted bytes from the memory stream.
+        return encrypted;
+    }
+
+    string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (cipherText == null || cipherText.Length <= 0)
+            throw new ArgumentNullException("cipherText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+
+        // Declare the string used to hold
+        // the decrypted text.
+        string plaintext = null;
+
+        // Create an Aes object
+        // with the specified key and IV.
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        return plaintext;
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AESCryptoTool;
+using System.Buffers.Text;
+using System.Text;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("***** AES Crypto Tool *****");
+Console.WriteLine("AES Crypto Tool");
+Console.WriteLine("Github: https://github.com/01hanybakir10/AESCryptoTool");
+Console.WriteLine("Copyright (c) 2007-2023 - CodeZEN");
+Console.WriteLine();
+
+while (true)
+{
+    Console.WriteLine("1. Encrypt string");
+    Console.WriteLine("2. Decrypt string ");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+    Console.Write("Enter your choice : ");
+    string choice = Console.ReadLine();
+
+    runTool rt = new();
+
+    switch (choice)
+    {
+        case "1":
+            rt.EncryptString();
+            break;
+        case "2":
+            rt.DecryptString();
+            break;
+        case "0":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+
+}
+
+public class runTool
+{
+    public string EncryptString()
+    {
+        CryptoHelper ch = new();
+
+        Console.Write("Enter string to encrypt : ");
+        string? plaintStr = Console.ReadLine();
+        Console.WriteLine("===============================");
+        string encStr = ch.AESencrypt(plaintStr, "", "");
+        Console.WriteLine(encStr);
+        Console.WriteLine("===============================");
+        return encStr;
+    }
+    public string DecryptString()
+    {
+        CryptoHelper ch = new();
+
+        Console.Write("Enter string to decrypt : ");
+        string? decStr = Console.ReadLine();
+        Console.WriteLine("===============================");
+        string encStr = ch.AESdecrypt(decStr, "", "");
+        Console.WriteLine(encStr);
+        Console.WriteLine("===============================");
+
+        return encStr;
+    }
+}
+
